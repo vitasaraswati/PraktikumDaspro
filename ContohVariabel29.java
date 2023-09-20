480c8ae6248f503b60378ed688fe544ddd407923
unknown <vitaekasaraswati16@gmail.com>
2023-09-20 11:43:28 +0700

    Percobaan 1 - Contoh Variabel 29

---
+++ ContohVariabel29.java
@@ -0,0 +1,19 @@
+public class ContohVariabel29 {
+    
+    public static void main(String[] args) {
+
+
+String salahSatuHobiSayaAdalah = "Bermain petak umpet";
+boolean isPandai = true;
+char jenisKelamin = 'L';
+byte _umurSayaSekarang = 20; 
+double $ipk = 3.24, tinggi = 1.78; 
+
+System.out.println(salahSatuHobiSayaAdalah);
+System.out.println("Apakah pandai? " + isPandai);
+System.out.println("Jenis Kelamin: " + jenisKelamin);
+System.out.println("IPK: " + $ipk);
+System.out.println(String.format("Saya berumur %s dengan tinggi badan %s", _umurSayaSekarang, tinggi));
+
+    }
+} 
