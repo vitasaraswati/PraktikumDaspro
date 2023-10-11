0501ac44c9fd835497d930ebc92d4529dbba8cb8
unknown <vitaekasaraswati16@gmail.com>
2023-10-11 11:49:34 +0700

    MODIFIKASI KABISAT NESTED 29

--- Praktikum06/KabisatNested29.java
+++ Praktikum06/KabisatNested29.java
@@ -10,10 +10,11 @@ public class KabisatNested29 {
     tahun = sc.nextInt ();
 
     if ((tahun % 4) == 0){
-        if ((tahun % 100) != 0)
+        if ((tahun % 100) != 0){
         System.out.println("Tahun Kabisat");
-    } else 
-    System.out.println(" Bukan Tahun Kabisat");
-
+        } else {
+        System.out.println(" Bukan Tahun Kabisat");
+        }
     }
+}
 }
