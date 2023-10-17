bd5783f477c92dab3dcdd00f08999a0937bc5105
unknown <vitaekasaraswati16@gmail.com>
2023-10-17 22:49:10 +0700

    implementasi bilangan29

--- Praktikum06/Bilangan29.java
+++ Praktikum06/Bilangan29.java
@@ -5,11 +5,18 @@ public class Bilangan29 {
     public static void main (String [] args) {
         Scanner sc = new Scanner (System.in);
     
-        int bil1, bil2, bil3
+        int bil1, bil2, bil3, bilanganmaks;
+        bil1 = 28;
+        bil2 = 54;
+        bil3 = 15;
 
-
-
+         if (bil1>bil2 && bil1>bil3) {
+            bilanganmaks = bil1;
+        } else if (bil2>bil1 && bil2>bil3){
+            bilanganmaks = bil2;
+        } else{
+            bilanganmaks = bil3;
+        }
+        System.out.println("Nilai terbesar adalah = " +bilanganmaks);
     }
-
-
 }
