a66f057c92cf2ad1b46172e23c3c5b1e5092e2c9
unknown <vitaekasaraswati16@gmail.com>
2023-10-17 21:41:37 +0700

    hasil modif kedua percobaan 1

--- Praktikum06/Kabisat29.java
+++ Praktikum06/Kabisat29.java
@@ -8,7 +8,7 @@ public class Kabisat29 {
     System.out.print("Masukkan tahun : ");
     tahun = sc.nextInt ();
 
-    if ((tahun % 4) == 0 && (tahun % 100) != 0){
+    if ((tahun % 4) == 0 && (tahun % 100) != 0 || (tahun % 400) ==0){
         System.out.println("Tahun Kabisat");
         } else {
         System.out.println(" Bukan Tahun Kabisat");
