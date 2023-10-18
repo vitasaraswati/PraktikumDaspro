42aee42ed802dad0bb67940704419ebac232791f
unknown <vitaekasaraswati16@gmail.com>
2023-10-18 12:35:12 +0700

    percobaan 1 - while kelipatan

---
+++ Praktikum07/WhileGaji29.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class WhileGaji29 {
+    public static void main (String[] args){
+    Scanner sc = new Scanner (System.in);
+
+    int jumlahKaryawan, jumlahLembur;
+    double gajiLembur = 0 , totalGajiLembur = 0;
+    String jabatan;
+    
+    System.out.print("Masukkan jumlah karyawan : ");
+    jumlahKaryawan = sc.nextInt();
+
+    int i = 0;
+
+    while (i < jumlahKaryawan) {
+        System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
+        System.out.print("Masukkan jabatan karyawan ke-" + (i+1) + ": ");
+        jabatan = sc.next();
+        System.out.print("Masukkan jumlah jam lembur :");
+        jumlahLembur = sc.nextInt();
+        i++;
+
+    if (jabatan.equalsIgnoreCase("direktur")) {
+        continue;
+    } else if (jabatan.equalsIgnoreCase("manajer")) {
+        gajiLembur = jumlahLembur * 100000;
+    }
+    totalGajiLembur += gajiLembur;
+}
+System.out.println("Total Gaji Lembur : " +totalGajiLembur);
+    
+    }     
+}
+    
--- Praktikum07/WhileKelipatan29.java
+++ Praktikum07/WhileKelipatan29.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-public class ForKelipatan29 {
+public class WhileKelipatan29 {
     public static void main (String[] args){
     Scanner sc = new Scanner (System.in);
-
+    
     int bilangan;
     int total = 0;
     int counter = 0;
@@ -11,12 +11,14 @@ public class ForKelipatan29 {
     System.out.print("Masukkan bilangan 1-9 : ");
     bilangan = sc.nextInt ();
 
-        for (int i = 0; i < 50; ++i){
-            if (i % bilangan == 0){
-                total += i;
-                ++counter;
-            }
+    int i = 0;
+    while (i < 50) {
+        if (i % bilangan == 0){
+            total += i;
+            counter++;
         }
+        i++;
+    }
     System.out.printf("Pada rentang 1-50 terdapat %d bilangan kelipatan %d dengan total %d", counter, bilangan, total);
     }
 }
