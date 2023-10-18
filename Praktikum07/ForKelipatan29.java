8f710159599508b64be4412af948eee477c69510
unknown <vitaekasaraswati16@gmail.com>
2023-10-18 11:17:49 +0700

    percobaan 1 forkelipatan

---
+++ Praktikum07/ForKelipatan29.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ForKelipatan29 {
+    public static void main (String[] args){
+    Scanner sc = new Scanner (System.in);
+
+    int bilangan;
+    int total = 0;
+    int counter = 0;
+
+    System.out.print("Masukkan bilangan 1-9 : ");
+    bilangan = sc.nextInt ();
+
+        for (int i = 0; i < 50; i++){
+            if (i % bilangan == 0){
+                total += i;
+                counter++;
+            }
+        }
+    System.out.printf("Pada rentang 1-50 terdapat %d bilangan kelipatan %d dengan total %d", counter, bilangan, total);
+    }
+}
