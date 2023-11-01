01dc226efa9cac97c63e311f0455a1ea7e461fdb
unknown <vitaekasaraswati16@gmail.com>
2023-11-01 11:12:44 +0700

    modifikasi percobaan 1

--- Praktikum09/ArrayBilangan29.java
+++ Praktikum09/ArrayBilangan29.java
@@ -4,16 +4,14 @@ public class ArrayBilangan29 {
     public static void main(String[] args) {
     Scanner sc = new Scanner (System.in);
 
-    int [] bil = new int [4];
-    bil[0] = 5;
-    bil[1] = 13;
-    bil[2] = -7;
-    bil[3] = 17;
+    double [] bil = new double [4];
+    bil[0] = 5.0;
+    bil[1] = 12867;
+    bil[2] = 7.5;
+    bil[3] = 2000000;
 
-    System.out.println(bil[0]);
-    System.out.println(bil[1]);
-    System.out.println(bil[2]);
-    System.out.println(bil[3]);
-    
+    for (int i = 0; i <=4; i++)
+        System.out.println(bil[i]);
+
     }
 }
