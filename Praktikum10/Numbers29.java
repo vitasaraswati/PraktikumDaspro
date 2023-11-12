e6455f86263a6a15364dee493f046c304dccb0a1
unknown <vitaekasaraswati16@gmail.com>
2023-11-12 17:06:34 +0700

    test 2

---
+++ Praktikum10/Numbers29.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Numbers29 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+
+    int [][] myNumbers = new int [3][];
+    myNumbers [0] = new int[5];
+    myNumbers [1] = new int[3];
+    myNumbers [2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke- " + (i+1) + ": " + myNumbers[i].length);
+        }
+    }
+}
