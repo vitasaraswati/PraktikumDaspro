187b7b910e6a456110437b32afbf3651b899b2ae
unknown <vitaekasaraswati16@gmail.com>
2023-11-15 11:28:03 +0700

    Percobaan 2 Square29

---
+++ Praktikum11/Square29.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Square29 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+
+    System.out.print("Masukkan jumlah *: ");
+    int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
