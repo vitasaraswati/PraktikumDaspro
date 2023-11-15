789132a1c76e8d0f2044cbaecc595e5501659192
unknown <vitaekasaraswati16@gmail.com>
2023-11-15 15:01:29 +0700

    Modifikasi Nested Loop

--- Praktikum11/NestedLoop29.java
+++ Praktikum11/NestedLoop29.java
@@ -17,11 +17,16 @@ public class NestedLoop29 {
 
         for (int i = 0; i < suhu.length; i++){
                 System.out.println("KOTA KE-" + (i+1) + ": ");
+
+                double totalSuhu = 0;
                 
-                for (int j = 0; j < suhu[i].length; j++){
-                System.out.print(suhu[i][j] + " ");
+                for (double temp : suhu[i]){
+                System.out.print(temp + " ");
+                totalSuhu += temp;
                 }   
 
+                double RataRata = totalSuhu / suhu[i].length;
+                System.out.println("\nRata - Rata suhu kota ke-" + (i+1) + ": " + RataRata);
                 System.out.println();
         }
     }
