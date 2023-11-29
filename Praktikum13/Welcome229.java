5883ba6fac3e7c800f30fc4b0953878f99f275b4
unknown <vitaekasaraswati16@gmail.com>
2023-11-29 11:14:57 +0700

    Welcome 2

---
+++ Praktikum13/Welcome229.java
@@ -0,0 +1,14 @@
+public class Welcome229 {
+
+    public static void displayWelcomeMessage(String name){
+        System.out.println("Selamat Datang di Politeknik Negeri Malang, " + name);
+    }
+
+    public static void main (String[] args){
+        displayWelcomeMessage("Ani");
+        displayWelcomeMessage("Budi");
+        displayWelcomeMessage("Cica");
+        displayWelcomeMessage("Dodi");
+    }
+}
+
