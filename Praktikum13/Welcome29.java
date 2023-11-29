aa3cf0010dd075f9bc64948f0549ce3c3dc3f6ba
unknown <vitaekasaraswati16@gmail.com>
2023-11-29 11:01:37 +0700

    Welcome 1

---
+++ Praktikum13/Welcome29.java
@@ -0,0 +1,10 @@
+public class Welcome29 {
+       
+        public static void displayWelcomeMessage(){
+            System.out.println("Selamat Datang di Politeknik Negeri Malang");
+        }
+
+        public static void main (String[] args){
+        displayWelcomeMessage(); 
+        }
+}
